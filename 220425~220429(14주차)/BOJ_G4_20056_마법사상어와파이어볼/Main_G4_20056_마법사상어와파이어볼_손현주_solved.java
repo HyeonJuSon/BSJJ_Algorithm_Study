9e20f855b3876e29de7834ac12f2926398cce03c
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-04-29 10:58:53 +0900

    [UPDATE] 20056 update

--- 220425~220429(14주차)/BOJ_G4_20056_마법사상어와파이어볼/Main_G4_20056_마법사상어와파이어볼_손현주_solved.java
+++ 220425~220429(14주차)/BOJ_G4_20056_마법사상어와파이어볼/Main_G4_20056_마법사상어와파이어볼_손현주_solved.java
@@ -1,204 +1,126 @@
 import java.io.*;
 import java.util.*;
 
-public class bj_23290_G1 {
+public class bj_20056_G4 {
 
-	static int M, S, N, maxSum;
-	static int[][] smell;
-	static ArrayList<Fish> map[][];
-	static int[][] sharkDir = { { -1, 0, 1, 0 }, { 0, -1, 0, 1 } }; // 상좌하우
-	static int[][] fishDir = { { 0, -1, -1, -1, 0, 1, 1, 1 }, { -1, -1, 0, 1, 1, 1, 0, -1 } };// ←, ↖, ↑, ↗, →, ↘, ↓, ↙
-	static ArrayList<Fish> storeFish;
-	static Fish shark;
-	static Queue<int[]> sharkMove = new LinkedList<>();
+	static int N, M, K;
+	static ArrayList<FireBall> fireballs;
+	static int[][] count;
+	static boolean gameOver;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		N = 4;
-		smell = new int[N][N];
-		map = new ArrayList[N][N];
-		for (int i = 0; i < N; ++i) {
-			Arrays.fill(smell[i], -1);
-			for (int j = 0; j < N; ++j)
-				map[i][j] = new ArrayList<>();
-		}
-		// M만큼 물고기 정보를 담는다.
-		for (int i = 0; i < M; ++i) {
+		K = Integer.parseInt(st.nextToken());
+		// fireball 입력받는다.
+		fireballs = new ArrayList<>();
+		count = new int[N][N];
+		gameOver = false;
+		for (int i = 0; i < M; ++i) { // 파볼 입력
 			st = new StringTokenizer(br.readLine(), " ");
 			int x = Integer.parseInt(st.nextToken()) - 1;
 			int y = Integer.parseInt(st.nextToken()) - 1;
-			int d = Integer.parseInt(st.nextToken()) - 1;
-			map[x][y].add(new Fish(x, y, d));
+			int m = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			fireballs.add(new FireBall(x, y, m, s, d));
 		}
-		// 상어의 위치를 받는다.
-		st = new StringTokenizer(br.readLine(), " ");
-		int sx = Integer.parseInt(st.nextToken()) - 1;
-		int sy = Integer.parseInt(st.nextToken()) - 1;
-		shark = new Fish(sx, sy, -1);
-		// S만큼 연습한다.
-		for (int s = 0; s < S; ++s) {
-			maxSum = -1; // 상어가 가장 많이 먹을 수 있는 물고기의 수
-			// 1. 예비 복제
-			beforeCopy();
-			// 2. 모든 물고기가 1칸 이동
-			moveFishes();
-			// 3. 상어가 연속 3칸으로 이동
-			moveShark(s);
-			// 4. s-2 번째 냄새 제거
-			deleteSmell(s);
-			// 5. 실제 복제
-			copy();
+		// K 만큼 수행
+		for (int i = 0; i < K; ++i) {
+			count = new int[N][N];
+			// 1. 모든 파이어 볼을 이동시킨다.
+			moveFireBalls();
+			// 2. 분열
+			sprayFireBalls();
 		}
-		// 격자의 물고기 수 .
+		// 질량의 합 출력
 		int answer = 0;
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				answer += map[i][j].size();
-			}
+		for (FireBall f : fireballs) {
+			answer += f.m;
 		}
 		System.out.println(answer);
 	}
 
-	static void deleteSmell(int s) {
-		if (s - 2 < 0)
-			return;
+	static void sprayFireBalls() {
+		ArrayList<FireBall> newFB = new ArrayList<>();
 		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < N; ++j) {
-				if (smell[i][j] != -1 && smell[i][j] == s - 2) {
-					smell[i][j] = -1;// 지워준다.
-				}
-			}
-		}
-	}
-
-	static void sharkDfs(int x, int y, int depth, Queue<int[]> log) {
-		if (depth == 3) {
-			// 물고기 먹을 수 있는거
-			Queue<int[]> l = new LinkedList<>(log);
-			boolean[][] eat = new boolean[N][N];
-			int sum = 0;
-			while (!l.isEmpty()) {
-				int[] now = l.poll();
-				if (eat[now[0]][now[1]])
-					continue;
-				eat[now[0]][now[1]] = true;
-				sum += map[now[0]][now[1]].size();
-			}
-			// 비교
-			if (maxSum < sum) {
-				// 갱신
-				maxSum = sum;
-				sharkMove = new LinkedList<>(log);
-			}
-			return;
-		}
-		for (int d = 0; d < 4; ++d) {
-			int nx = x + sharkDir[0][d];
-			int ny = y + sharkDir[1][d];
-			if (!isBoundary(nx, ny))
-				continue;
-			Queue<int[]> tmp = new LinkedList<>(log);
-			tmp.add(new int[] { nx, ny });
-			sharkDfs(nx, ny, depth + 1, tmp);
-		}
-	}
-
-	static void moveShark(int s) {
-		sharkDfs(shark.x, shark.y, 0, new LinkedList<>());
-		// 실제로 상어를 움직인다.
-		while (!sharkMove.isEmpty()) {
-			int[] now = sharkMove.poll();
-			if (map[now[0]][now[1]].size() > 0) { // 이동 칸에 물고기가 있으면 모두 제외
-				map[now[0]][now[1]].clear(); // 물고기 지움
-				smell[now[0]][now[1]] = s;// 냄새를 남김
-			}
-			if (sharkMove.isEmpty()) {
-				shark.x = now[0];
-				shark.y = now[1];
-			}
-		}
-	}
-
-	static void moveFishes() {
-		Queue<Fish> moveList = new LinkedList<>();
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				if (map[i][j].size() == 0)
-					continue; // 물고기 없으면 패스한다.
-				for (int k = 0; k < map[i][j].size(); ++k) {
-					Fish f = map[i][j].get(k);
-					int nx = f.x + fishDir[0][f.dir];
-					int ny = f.y + fishDir[1][f.dir];
-					// 이중 하나라도 조건에 걸리면 반시계 회전시킴
-					int cnt = 1;
-					if (!isBoundary(nx, ny) || smell[nx][ny] != -1 || (shark.x == nx && shark.y == ny)) {
-						int nDir = f.dir;
-						while (true) {
-							nDir = (nDir - 1 + 8) % 8;// 반시계회전한다.
-							nx = f.x + fishDir[0][nDir];
-							ny = f.y + fishDir[1][nDir];
-							if (isBoundary(nx, ny) && smell[nx][ny] == -1 && (nx != shark.x || ny != shark.y)) {
-								f.dir = nDir;
-								break;
-							}
-							if (++cnt == 8)
-								break;
+				if (count[i][j] >= 2) { // 카운트가 2이상이면
+					int fCnt = 0; // 파볼 갯수
+					int fMassSum = 0; // 파볼들의 질량의 합
+					int fSpeedSum = 0; // 파볼들의 스피드 합
+					int fDirCheck[] = new int[8];
+					for (int c=0;c<fireballs.size();++c) {
+						FireBall f = fireballs.get(c);
+						if (f.x == i && f.y == j) {
+							++fCnt;
+							fSpeedSum += f.s;
+							fDirCheck[f.d]++;
+							fMassSum += f.m;
+							fireballs.remove(c);
+							--c;
 						}
 					}
-					// cnt 가 8이면 갱신하지 않는다.
-					if (cnt == 8)
+
+					int nm = fMassSum / 5;
+					if (nm == 0)
 						continue;
-					// 갱신한다.
-					moveList.add(new Fish(nx, ny, f.dir));// 새위치에 추가하고
-					map[f.x][f.y].remove(k);// 기존 위치에서는 지워준다.
-					--k; // 리스트 사이즈가 줄어들기 때문에 현재 인덱스를 줄인다.
-				}
-			}
-		}
-		// 물고기를 이동시킨다.
-		while (!moveList.isEmpty()) {
-			Fish f = moveList.poll();
-			map[f.x][f.y].add(new Fish(f.x, f.y, f.dir));
-		}
-	}
-
-	static boolean isBoundary(int x, int y) {
-		return 0 <= x && x < N && 0 <= y && y < N;
-	}
-
-	static void beforeCopy() {
-		storeFish = new ArrayList<>();
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				if (map[i][j].size() > 0) {
-					for (Fish f : map[i][j]) {
-						storeFish.add(new Fish(i, j, f.dir));
+					int ns = fSpeedSum / fCnt;
+					int nd = getDir(fDirCheck);
+					for (int nf = 0; nf < 4; ++nf) {
+						newFB.add(new FireBall(i, j, nm, ns, nd));
+						nd += 2;
 					}
 				}
 			}
 		}
-	}
-
-	static void copy() {
-		// 저장해두었던 물고기들을 복제한다.
-		for (Fish f : storeFish) {
-			map[f.x][f.y].add(new Fish(f.x, f.y, f.dir));
+		for(int i=0;i<newFB.size();++i) {
+			FireBall now = newFB.get(i);
+			fireballs.add(new FireBall(now.x,now.y,now.m,now.s,now.d));
 		}
 	}
 
-	// class =================================================================
-	
-	static class Fish {
-		int x, y;
-		int dir;
+	static int getDir(int[] tmp) {
+		boolean o = false, e = false;
+		for (int i = 0; i < 8; ++i) {
+			if ((i + 1) % 2 == 0 && tmp[i] != 0)
+				e = true;
+			else if ((i + 1) % 2 != 0 && tmp[i] != 0)
+				o = true;
+		}
+		return o && e ? 1 : 0;
+	}
 
-		public Fish(int x, int y, int dir) {
+	static int[][] dir = { { -1, -1, 0, 1, 1, 1, 0, -1 }, { 0, 1, 1, 1, 0, -1, -1, -1 } };
+
+	static void moveFireBalls() {
+		ArrayList<FireBall> tmp = new ArrayList<>();
+		for (int i = 0; i < fireballs.size(); ++i) {
+			FireBall now = fireballs.get(i);
+			int nx = (now.x + N + (dir[0][now.d] * (now.s % N))) % N;
+			int ny = (now.y + N + (dir[1][now.d] * (now.s % N))) % N;
+			count[nx][ny]++;
+			tmp.add(new FireBall(nx, ny, now.m, now.s, now.d));
+		}
+		fireballs = tmp; // 갱신.
+	}
+
+	static class FireBall {
+		int x, y;
+		int m;
+		int s;
+		int d;
+
+		public FireBall(int x, int y, int m, int s, int d) {
 			this.x = x;
 			this.y = y;
-			this.dir = dir;
+			this.m = m;
+			this.s = s;
+			this.d = d;
 		}
+
 	}
+
 }
